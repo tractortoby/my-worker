04b26163de0dfb31bd015e1aad6090f802a26aa5
tractortoby <tobychung1993@gmail.com>
2022-08-13 15:10:41 +0800

    Add country-code-emoji

--- template.js
+++ template.js
@@ -1,16 +1,21 @@
-const template = () => `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <h1>Hello Worker!</h1>
-</body>
-</html>
-`;
+import flag from 'country-code-emoji'
 
-export default template;
+const template = cf => {
+  const emoji = flag(cf.country) || '👋🏻'
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+      </head>
+      <body>
+        <h1>Hello there! You're connecting from ${cf.country} ${emoji}.</h1>
+      </body>
+    </html>
+  `
+}
+
+export default template
