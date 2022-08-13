117def9e40f2aa7b19428578094ca73a267cf818
tractortoby <tobychung1993@gmail.com>
2022-08-13 15:17:41 +0800

    Add CSS

--- template.js
+++ template.js
@@ -2,6 +2,7 @@ import flag from 'country-code-emoji'
 
 const template = cf => {
   const emoji = flag(cf.country) || '👋🏻'
+  
   return `
     <!DOCTYPE html>
     <html lang="en">
@@ -9,10 +10,30 @@ const template = cf => {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>my-worker</title>
+
+        <link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css" />
+
+        <style>
+          body {
+            background: #eee;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            min-height: 100vh;
+            font-family: sans-serif;
+          }
+          div.container {
+            background: #fff;
+            border-radius: 1rem;
+            padding: 4rem;
+          }
+        </style>
       </head>
       <body>
-        <h1>Hello there! You're connecting from ${cf.country} ${emoji}.</h1>
+        <div class="container">
+          <h1>Hello there! You're connecting from ${cf.country} ${emoji}.</h1>
+        </div>
       </body>
     </html>
   `
